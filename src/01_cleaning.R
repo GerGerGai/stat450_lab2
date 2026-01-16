1ea525ec4203a56ea40a997dce973d486a170f76
Jerry Gai <jerrygai@dhcp-206-12-167-12.ubcsecure.wireless.ubc.ca>
2026-01-16 15:24:08 -0800

    lab2 commit

--- src/01_cleaning.R
+++ src/01_cleaning.R
@@ -7,12 +7,12 @@ library(janitor)
 # Fill in your code where necessary
 
 # Read the raw data
-raw_dat <- read_csv(...)
+raw_dat <- read_csv(here("data", "raw_data.csv"))
 
 # Wrangle the data
 new_dat <- raw_dat %>% 
-  clean_... %>% 
-  filter(...)
+  clean_names() %>%
+  filter(temperature_celsius > 10)
 
 # Save the processed data
-write_rds(new_dat, ...)
+write_rds(new_dat, here("data", "processed_data.rds"))
